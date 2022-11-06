be2092e783231a87cfba00a724e9fe1cb6729045
Christof Dorner <christof@chdorner.com>
2022-11-06 23:16:15 +0100

    Render keycaps with labels

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/fsnotify/fsnotify v1.6.0
 	github.com/gorilla/websocket v1.5.0
 	github.com/santhosh-tekuri/jsonschema/v5 v5.0.2
+	github.com/satori/go.uuid v1.2.0
 	github.com/sirupsen/logrus v1.9.0
 	github.com/spf13/cobra v1.6.1
 	github.com/spf13/pflag v1.0.5
--- internal/keytographer/renderer.go
+++ internal/keytographer/renderer.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	svg "github.com/ajstarks/svgo"
+	uuid "github.com/satori/go.uuid"
 )
 
 type Renderer interface {
@@ -23,13 +24,17 @@ func (r *renderer) Render(c *Config) []byte {
 
 	s := svg.New(buf)
 	s.Start(c.Canvas.Width, c.Canvas.Height)
-	s.Def()
-	r.keycap(s)
-	s.DefEnd()
 	s.Style("text/css", r.styles(c))
 
-	s.Use(10, 10, "#keycap")
-	s.Use(90, 10, "#keycap")
+	r.keycap(s, keycapOptions{10, 10, "S"})
+	r.keycap(s, keycapOptions{85, 10, "T"})
+	r.keycap(s, keycapOptions{160, 10, "R"})
+	r.keycap(s, keycapOptions{235, 10, "A"})
+
+	r.keycap(s, keycapOptions{10, 85, "O"})
+	r.keycap(s, keycapOptions{85, 85, "I"})
+	r.keycap(s, keycapOptions{160, 85, "Y"})
+	r.keycap(s, keycapOptions{235, 85, "E"})
 
 	s.End()
 
@@ -49,9 +54,26 @@ svg {
 	return styles
 }
 
-func (r *renderer) keycap(s *svg.SVG) {
-	s.Gid("keycap")
-	s.Roundrect(0, 0, 70, 70, 3, 3, "fill=\"#383838\"")
-	s.Roundrect(7, 6, 56, 56, 3, 3, "fill=\"#FFFFFF\"", "fill-opacity=\"0.1\"")
+type keycapOptions struct {
+	x     int
+	y     int
+	label string
+}
+
+func (r *renderer) keycap(s *svg.SVG, opts keycapOptions) {
+	inx, iny, inw, inh := opts.x+7, opts.y+6, 56, 56
+	fontSize := 16
+	s.Gid(fmt.Sprintf("keycap-%s", uuid.NewV4().String()))
+	s.Roundrect(opts.x, opts.y, 70, 70, 3, 3, "fill=\"#383838\"")
+	s.Roundrect(inx, iny, inw, inh, 2, 2, "fill=\"#FFFFFF\"", "fill-opacity=\"0.1\"")
+	s.Text(
+		inx+(inw/2),
+		iny+(inh/2)+(fontSize/3),
+		opts.label,
+		"font-family=\"Arial\"",
+		fmt.Sprintf("font-size=\"%d\"", fontSize),
+		"fill=\"#e3e3e3\"",
+		"text-anchor=\"middle\"",
+	)
 	s.Gend()
 }
