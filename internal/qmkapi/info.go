18c7fc2bc18fa1dc9cc44cda419346c7932687bc
Christof Dorner <christof@chdorner.com>
2022-11-08 00:19:21 +0100

    Add simple client for QMK API

---
+++ internal/qmkapi/info.go
@@ -0,0 +1,48 @@
+package qmkapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type KeyboardInfo struct {
+	Keyboards map[string]Keyboard
+}
+
+type Keyboard struct {
+	KeyboardName string `json:"keyboard_name"`
+	Layouts      map[string]Layout
+}
+
+type Layout struct {
+	Keys []Key `json:"layout"`
+}
+
+type Key struct {
+	X float32
+	Y float32
+	W float32
+	H float32
+}
+
+func Info(keyboard, path string) (*KeyboardInfo, error) {
+	resp, err := http.Get(fmt.Sprintf(`https://keyboards.qmk.fm/v1/keyboards/%s/%s/info.json`, keyboard, path))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var info KeyboardInfo
+	err = json.Unmarshal(body, &info)
+	if err != nil {
+		return nil, err
+	}
+	return &info, nil
+}
